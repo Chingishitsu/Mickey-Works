cafc4631a16789cf4f35d974b463b47dd3b48e3a
katsu <qq259487515@gmail.com>
2018-02-21 18:57:09 +0900

    first git

--- database/migrations/2018_02_20_053426_create_companies_table.php
+++ database/migrations/2018_02_20_053426_create_companies_table.php
@@ -1,7 +1,9 @@
 <?php
+
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+
 class CreateCompaniesTable extends Migration
 {
     /**
@@ -13,28 +15,29 @@ class CreateCompaniesTable extends Migration
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->increments('id');
-
-            $table->string('username',255)->unique();
-
+            
+            $table->string('username',255);
+            
             $table->string('password',255);
-
-            $table->string('email',255)->unique();
-
-            $table->string('name',255)->unique();
-
+            
+            $table->string('email',255);
+            
+            $table->string('name',255);
+            
             $table->longText('address')->nullable();
-
+            
             $table->integer('mst_csub_id')->unsigned();
             $table->foreign('mst_csub_id')->references('id')->on('mst_csubs')->onDelete('cascade');
-
+            
             $table->longText('message')->nullable();
-
+            
             $table->integer('money')->nullable();
-
+            
             $table->timestamps();
             $table->softDeletes();
         });
     }
+
     /**
      * Reverse the migrations.
      *
--- database/migrations/2018_02_20_053549_create_matching_table.php
+++ database/migrations/2018_02_20_053549_create_matching_table.php
@@ -1,7 +1,9 @@
 <?php
+
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+
 class CreateMatchingTable extends Migration
 {
     /**
@@ -13,18 +15,25 @@ class CreateMatchingTable extends Migration
     {
         Schema::create('matching', function (Blueprint $table) {
             $table->increments('id');
+
             $table->integer('student_id')->unsigned();
             $table->foreign('student_id')->references('id')->on('students')->onDelete('cascade');
+
             $table->integer('company_id')->unsigned();
             $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
+
             $table->longText('student_comment')->nullable();
+
             $table->longText('company_comment')->nullable();
+
             $table->integer('result_id')->unsigned();
             $table->foreign('result_id')->references('id')->on('mst_results')->onDelete('cascade');
+
             $table->timestamps();
             $table->softDeletes();
         });
     }
+
     /**
      * Reverse the migrations.
      *
