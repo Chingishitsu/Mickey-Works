ed629d1f46210d91f9a76a040005db2694fb9469
katsu <qq259487515@gmail.com>
2018-02-23 14:30:47 +0900

    adminmatch 1

--- app/Match.php
+++ app/Match.php
@@ -6,6 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Match extends Model
 {
+  public static $rules = array(
+    'student_id' => 'required',
+    'company_id' => 'required',
+    'student_comment' => 'between:0,500',
+  );
+  public static $messages = array(
+    'student_id.required' => '留学を選択してください',
+    'company_id.required' => '会社を選択してください',
+    'student_comment.between' => '最大500文字'
+  );
+
   use SoftDeletes;
   protected $dates = ['deleted_at'];
   protected $table = 'matching';
