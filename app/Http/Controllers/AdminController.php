0125420e79b81c5a8be606b7667f5619d1c59ca1
cui1986 <35281420+cui1986@users.noreply.github.com>
2018-02-23 13:41:00 +0900

    cui223

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class AdminController extends Controller
+{
+  public function (Request $request)
+  {
+      return view('');
+  }
+  public function (Request $request)
+  {
+
+
+  }
+}
