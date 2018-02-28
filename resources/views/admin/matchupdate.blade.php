38ae7cc059ceecf228a5ec5200295b6349a946ff
katsu <qq259487515@gmail.com>
2018-02-28 17:05:11 +0900

    2.28 1

--- resources/views/admin/matchupdate.blade.php
+++ resources/views/admin/matchupdate.blade.php
@@ -3,39 +3,95 @@
 @section('content')
 <div class="container" style="margin-top:50px">
   <h2>マッチングの情報編集画面</h2>
-  <form>
+  <form action="" method="post">
+    {{ csrf_field() }}
     <div class="form-group">
-      <label>企業ID</label>
-      <input type="text" class="form-control" id="name" name="">
+      <span>留学生ID</span><span style="color: #bd2130">{{$errors->first('student_id')}}</span>
+      <select class="form-control" name="student_id">
+        @if ($_SERVER['REQUEST_METHOD'] = "get")
+          @foreach($students as $student)
+            @php
+            $select = ($item->student_id == $student->id)? "selected":"";
+            @endphp
+          <option value="{{$student->id}}" {{$select}}>{{$student->id.'|'.$student->name.'|'.$student->email}}</option>
+          @endforeach
+        @else
+          @foreach($students as $student)
+            @php
+              $select = (old('student_id') == $student->id)? "selected":"";
+            @endphp
+            <option value="{{$student->id}}" {{$select}}>{{$student->name.'|'.$student->email}}</option>
+          @endforeach
+         @endif
+      </select>
     </div>
 
     <div class="form-group">
-      <label>留学生ID</label>
-      <input type="password" class="form-control" id="pwd">
+      <span>企業ID</span><span style="color: #bd2130">{{$errors->first('company_id')}}</span>
+      <select class="form-control" name="company_id">
+        @if ($_SERVER['REQUEST_METHOD'] = "get")
+          @foreach($companies as $company)
+            @php
+              $select = ($item->company_id == $company->id)? "selected":"";
+            @endphp
+            <option value="{{$company->id}}" {{$select}}>{{$company->id.'|'.$company->name}}</option>
+          @endforeach
+        @else
+          @foreach($students as $student)
+            @php
+              $select = (old('company_id') == $company->id)? "selected":"";
+            @endphp
+            <option value="{{$company->id}}" {{$select}}>{{$cpmpany->id.'|'.$company->name}}</option>
+          @endforeach
+        @endif
+      </select>
     </div>
 
     <div class="form-group">
-      <label>留学生アピール</label>
-      <textarea class="form-control" rows="5" id="message"></textarea>
+      <span>学生アピール</span><span style="color: #bd2130">{{$errors->first('student_comment')}}</span>
+<textarea class="form-control" rows="5" name="student_comment">
+@if (old('student_comment') != null)
+{{old('student_comment')}}
+@else
+{{$item->student_comment}}
+@endif
+</textarea>
     </div>
 
     <div class="form-group">
       <label>企業コメント</label>
-      <textarea class="form-control" rows="5" id="message"></textarea>
+<textarea class="form-control" rows="5" name="company_comment">
+@if (old('company_comment') != null)
+{{old('company_comment')}}
+@else
+{{$item->company_comment}}
+@endif
+</textarea>
     </div>
 
     <div class="form-group">
       <label>マッチング結果</label>
-      <select class="form-control" id="csub">
-        <option>未読</option>
-        <option>合格</option>
-        <option>未定</option>
-        <option>不合格</option>
+      <select class="form-control" name="result_id">
+       @if (old('result_id') != null)
+          @foreach($results as $result)
+            @php
+              $select = (old('result_id') == $result->id)? "selected":"";
+            @endphp
+            <option value="{{$result->id}}" {{$select}}>{{$result->name}}</option>
+          @endforeach
+       @else
+          @foreach($results as $result)
+            @php
+              $select = ($item->result_id == $result->id)? "selected":"";
+            @endphp
+            <option value="{{$result->id}}" {{$select}}>{{$result->name}}</option>
+          @endforeach
+       @endif
       </select>
     </div>
 
     <button type="submit" class="btn btn-primary">提出</button>
-    <button type="reset" class="btn btn-primary">リセット</button>
+    <button><a href="{{url('admin/matchindex')}}">back</a></button>
   </form>
 </div>
 @endsection
